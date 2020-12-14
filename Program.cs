20fa9f3121d9ec061fcf82cf0aa50c7dec5d0334
Taila <taila.franco@aluno.ufabc.edu.br>
2020-12-14 08:47:57 -0300

    Adicionado conversor euro

--- Program.cs
+++ Program.cs
@@ -10,6 +10,9 @@ namespace classe_estatica
             Console.WriteLine($"Conversor de Dolar para reais, sendo 4 doláres, o resultado será: \n  Resultado:{Conversor.DolarParaReais(4f)} RS");
 
             Console.WriteLine($"Conversor de reais para dolares, sendo 50 reais, o resultado será: \n  Resultado:{Conversor.RealParaDolar(50f)} US");
+            Console.WriteLine($"Conversor de Euro para reais, sendo 8 euros, o resultado será: \n  Resultado:{Conversor.EuroParaReais(8f)} ER");
+
+            Console.WriteLine($"Conversor de reais para euros, sendo 70 reais, o resultado será: \n  Resultado:{Conversor.RealParaEuro(70f)} RS");
         }
     }
 }
--- classes/Conversor.cs
+++ classes/Conversor.cs
@@ -4,6 +4,7 @@ namespace classe_estatica.classes
     public static class Conversor
     {
         private static float cotacaoDolar = 5.07f;
+        private static float cotacaoEuro = 6.16f;
         public static float RealParaDolar(float valorRS)
         {
             return valorRS / cotacaoDolar;
@@ -11,6 +12,14 @@ namespace classe_estatica.classes
         public static float DolarParaReais(float valorUS)
         {
             return cotacaoDolar * valorUS;
+        }
+        public static float RealParaEuro(float valorRS)
+        {
+            return valorRS / cotacaoEuro;
+        }
+        public static float EuroParaReais(float valorER)
+        {
+            return cotacaoEuro * valorER;
         }
     }   
 }
